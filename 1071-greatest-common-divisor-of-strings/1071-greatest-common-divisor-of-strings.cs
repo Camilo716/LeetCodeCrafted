473ff8ee19c8557bd6c94501a5a054730cc62862
Camilo Gonzalez <105132863+Camilo716@users.noreply.github.com>
2023-10-23 10:23:55 -0500

    Time: 71 ms (74.91%), Space: 38.2 MB (50.53%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cs
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cs
@@ -1,10 +1,8 @@
 public class Solution {
     public string GcdOfStrings(string str1, string str2) {
-        if(!(str1 + str2).Equals(str2 + str1))
-            return "";
-
-        int gcd = GetGCD(str1.Length, str2.Length);
-        return str1.Substring(0, gcd);
+        return (!(str1 + str2).Equals(str2 + str1))
+            ? ""
+            : str1.Substring(0, GetGCD(str1.Length, str2.Length));
     }
 
     private int GetGCD(int a, int b)
