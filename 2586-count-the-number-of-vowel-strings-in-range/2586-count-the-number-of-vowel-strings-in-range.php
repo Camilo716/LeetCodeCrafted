47fdf21b21fbf714a72436356f3836b0fd6a8589
Camilo Gonzalez <105132863+Camilo716@users.noreply.github.com>
2023-12-22 16:17:00 -0500

    Time: 20 ms (66.67%), Space: 19.2 MB (66.67%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.php
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.php
@@ -8,21 +8,27 @@ class Solution {
      */
     function vowelStrings($words, $left, $right) {
         $wordsInRange = array_slice($words, $left, $right - $left + 1);
-        $vowels = ['a', 'e', 'i', 'o', 'u'];
         $numberOfVowelStrings = 0;
 
         foreach($wordsInRange as $word)
         {
-            $firstChar = strtolower($word[0]);
-            $lastChar = strtolower($word[strlen($word) - 1]);
-
-            $startsWithVowel = in_array($firstChar, $vowels);
-            $endsWithVowel = in_array($lastChar, $vowels);
-
-            if($startsWithVowel && $endsWithVowel)
+            if($this->isVowelString($word))
                 $numberOfVowelStrings++;
         }
 
         return $numberOfVowelStrings;
+    }
+
+    function isVowelString($word)
+    {
+        $vowels = ['a', 'e', 'i', 'o', 'u'];
+
+        $firstChar = strtolower($word[0]);
+        $lastChar = strtolower($word[strlen($word) - 1]);
+
+        $startsWithVowel = in_array($firstChar, $vowels);
+        $endsWithVowel = in_array($lastChar, $vowels);
+
+        return $startsWithVowel && $endsWithVowel;
     }
 }
