38de18b6b56548e813913c46939bc9624cc0424e
Camilo Gonzalez <105132863+Camilo716@users.noreply.github.com>
2024-01-21 20:53:46 -0500

    Time: 84 ms (86.89%), Space: 45.9 MB (9.84%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cs
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cs
@@ -1,13 +1,9 @@
 public class Solution {
     public string KthDistinct(string[] arr, int k) {
-        string result = "";
-
-        var wordsThatAppearsOnlyOnce = arr.Where(word => arr.Count(w => w == word) == 1);
-        
-        bool kNotPresent = k > wordsThatAppearsOnlyOnce.Count();
-        if(!kNotPresent)
-            result = wordsThatAppearsOnlyOnce.ElementAt(k - 1);
-
-        return result;
+        return arr
+            .GroupBy(word => word)
+            .Where(group => group.Count() == 1)
+            .Select(group => group.Key)
+            .ElementAtOrDefault(k-1) ?? "";
     }
 }
