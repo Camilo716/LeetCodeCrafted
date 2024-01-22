962775295b0619f13f4c7a4a8f2efd7b6dace000
Camilo Gonzalez <105132863+Camilo716@users.noreply.github.com>
2024-01-22 09:42:12 -0500

    Time: 71 ms (99.73%), Space: 43.5 MB (5.18%) - LeetHub

--- 1436-destination-city/1436-destination-city.cs
+++ 1436-destination-city/1436-destination-city.cs
@@ -1,15 +1,8 @@
 public class Solution {
     public string DestCity(IList<IList<string>> paths) {
-        List<string> cities = paths.SelectMany(path => path).Distinct().ToList();   
-    
-        foreach(var path in paths)
-        {
-            string? firstCityInPath = cities.FirstOrDefault(city => city == path[0]);
+        var cities = paths.SelectMany(path => path);
+        var firstCitiesByPath = paths.Select(path => path[0]);   
 
-            if(firstCityInPath != null)
-                cities.Remove(firstCityInPath);
-        }
-
-        return cities.FirstOrDefault();
+        return cities.Except(firstCitiesByPath).FirstOrDefault();
     }
 }
