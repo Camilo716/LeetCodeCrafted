34aafdda0f7b17edb580b0c0ceda39f77d4a3819
Camilo Gonzalez <105132863+Camilo716@users.noreply.github.com>
2024-02-21 21:30:12 -0500

    Time: 44 ms (65.40%), Space: 38.5 MB (22.75%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cs
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cs
@@ -1,13 +1,7 @@
 public class Solution {
     public int NumJewelsInStones(string jewels, string stones) {
-        int jewelsFound = 0;
-
-        foreach(char stone in stones)
-        {
-            if(jewels.Contains(stone))
-                jewelsFound++;
-        }
-
-        return jewelsFound;
+        return stones
+            .ToCharArray()
+            .Count(stone => jewels.Contains(stone));
     }
 }
