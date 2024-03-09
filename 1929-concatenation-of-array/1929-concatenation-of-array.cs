9b17570f28497cca0f5e9ba3696b4e461e0fc3ea
Camilo Gonzalez <105132863+Camilo716@users.noreply.github.com>
2024-03-08 22:25:35 -0500

    Time: 120 ms (52.74%), Space: 51 MB (50.45%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        var ans = new int[nums.Length * 2];
+
+        for(int i = 0; i < ans.Length; i++)
+            ans[i] = nums[i % nums.Length];
+
+        return ans;
+    }
+}
