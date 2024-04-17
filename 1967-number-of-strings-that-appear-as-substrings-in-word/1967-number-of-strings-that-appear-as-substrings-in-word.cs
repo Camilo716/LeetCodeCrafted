69b0acafd1404343cd78f417b55037b6a622c7c4
Camilo Gonzalez <105132863+Camilo716@users.noreply.github.com>
2024-04-16 20:07:44 -0500

    Time: 63 ms (46.15%), Space: 40.3 MB (71.79%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cs
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public int NumOfStrings(string[] patterns, string word) {
         return patterns
+            .ToList()
             .Count(pattern => word.Contains(pattern));
     }
 }
