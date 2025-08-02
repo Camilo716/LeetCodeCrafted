0b37ea8e1f43a7c912ccbd607ad71799e823bb08
Camilo Gonzalez <105132863+Camilo716@users.noreply.github.com>
2025-08-02 13:50:15 -0500

    Time: 1 ms (7.02%), Space: 46.6 MB (95.58%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int k = 0;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] != val)
+            {
+                k++;
+                continue;
+            }
+
+            nums[i] = int.MaxValue;
+        }
+
+        Array.Sort(nums);
+
+        return k;
+    }
+}
