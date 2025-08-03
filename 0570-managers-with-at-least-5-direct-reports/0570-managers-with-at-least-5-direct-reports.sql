20a7c363d8a88d0f501a98c042af7bc5ef188fb6
Camilo Gonzalez <105132863+Camilo716@users.noreply.github.com>
2025-08-03 11:12:37 -0500

    Time: 1583 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,18 +1,14 @@
 /* Write your T-SQL query statement below */
 
 SELECT 
-    out_employee.Name
-FROM (
+    Name
+FROM employee
+WHERE id IN (
     SELECT 
-        manager.managerId,
-        COUNT(1) as employees_per_manager
-    FROM employee AS manager
-        LEFT JOIN employee ON employee.id = manager.id
-    WHERE manager.managerId IS NOT NULL
-    GROUP BY manager.managerId
-) as m
-JOIN 
-    employee as out_employee ON out_employee.id = m.managerId
-WHERE m.employees_per_manager >= 5
+        managerId
+    FROM employee
+    GROUP BY managerId
+    HAVING count(1) >= 5
+)
 
 
